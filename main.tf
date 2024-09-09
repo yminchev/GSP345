8a77986f49ad06427c409087e5526e370295aae5
yminchev <44697684+yminchev@users.noreply.github.com>
2024-09-09 19:17:12 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
 
+provider "google" {
+
+  project = "PROJECT ID"
+  region  = "REGION"
+  zone    = "ZONE"
+}
