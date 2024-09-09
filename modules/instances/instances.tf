fe54f9a4877046635a611c602a5405d4d1979e8f
yminchev <44697684+yminchev@users.noreply.github.com>
2024-09-09 19:15:52 +0300

    Update instances.tf

--- modules/instances/instances.tf
+++ modules/instances/instances.tf
@@ -1,1 +1,18 @@
+resource "google_compute_instance" "terraform" {
+  project      = ""
+  name         = "terraform"
+  machine_type = "e2-medium"
+  zone         = ""
 
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
